28ca766f59100b57b1aa4eebefef29f1c2811791
AliNamooya <ali.namooya1@gmail.com>
2021-11-26 22:19:38 +0100

    Utilisateur OK
    SignIn OK
    LogIn OK

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,12 +1,51 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/User');
 
-exports.signup = (req, res, next) => {
-  const user = new User({
-    ...req.body
-  });
-  //enregistrer le schema dans la base de donnée
-  //les informations sont visible sur le cluster dns collections
-  user.save()
-  .then(() => res.status(201).json({message: 'objet enregistrer'}))
-  .catch(error => res.status(400).json({error}));
+
+
+exports.signup = (req, res, next) => { 
+  bcrypt.hash(req.body.password, 10)
+  .then(hash =>{
+    const user = new User({
+      email: req.body.email,
+      password: hash
+    });
+    //sauvegarde des infos dans la BDD atlas
+    user.save()
+    .then(() => res.status(201).json({ message: 'Utilisateur créé !'}))
+    .catch(error => res.status(400).json({ error }));
+  })
+  .catch(error => res.status(500).json({ error }));
+};
+
+
+
+
+exports.login = (req, res, next) => {
+  User.findOne({ email : req.body.email }) // récupération de l'email entrée dans la BDD
+  .then(user => {
+    if (!user){//si l'email n'est pas valable
+      return res.statu(401).json({error: 'Utilisateur non trouvé'})
+    }
+    //si c'est valable, on compare le mdp avec celui de la BDD
+    bcrypt.compare(req.body.password, user.password)
+    .then(valid => {
+      if (!valid) {// si le mdp n'est pas valable
+        return res.statu(401).json({error: 'Mot de passe incorrecte'})
+      }
+      //si c'est valable, on lui renvoie les infos suivantes
+      res.status(200).json({
+        userId: user._id,
+        token: jwt.sign(
+          { userId: user._id},
+          'RANDOM_TOKEN_SECRET',
+          { expiresIn: '24h'} //token valable 24h
+        )
+      });
+    })
+    .catch(error => res.status(500).json({}));
+  })
+  .catch(error => res.status(500).json({}));
 };
